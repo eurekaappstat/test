d757d2e8dca3db9384c831a21e2e46057526ba34
manjunathnbec <manjunathnbec@gmail.com>
2015-08-19 21:04:26 +0530

    Removing check for whatsapp, face book , contcts
    Removing check for whatsapp, face book , contcts

--- app/src/main/java/com/example/manjunathnb/appstats/AppMoniterService.java
+++ app/src/main/java/com/example/manjunathnb/appstats/AppMoniterService.java
@@ -13,49 +13,49 @@ import android.util.Log;
  * Created by manjunathnb on 7/13/2015.
  */
 public class AppMoniterService extends Service {
-	public static final String ID = "_id";
-	public static final String APP_PACKAGE = "app_package";
-	public static final String LAST_APP_LAUNCHED_TIME = "last_app_launched_time";
-	public static final String LAST_APP_EXITED_TIME = "last_app_exited_time";
+    public static final String ID = "_id";
+    public static final String APP_PACKAGE = "app_package";
+    public static final String LAST_APP_LAUNCHED_TIME = "last_app_launched_time";
+    public static final String LAST_APP_EXITED_TIME = "last_app_exited_time";
 
-	ActivityManager mActivityManager;
-	private AppMoniterThread mAppMoniterThread;
-	String previousPackageName = "";
-	long previousinsertedID = 0;
+    ActivityManager mActivityManager;
+    private AppMoniterThread mAppMoniterThread;
+    String previousPackageName = "";
+    long previousinsertedID = 0;
 
-	@Override
-	public void onCreate() {
-		startThread();
-		mActivityManager = (ActivityManager) getBaseContext().getSystemService(Context.ACTIVITY_SERVICE);
-		Log.i("manju", "onCreate of service");
-		super.onCreate();
-	}
+    @Override
+    public void onCreate() {
+        startThread();
+        mActivityManager = (ActivityManager) getBaseContext().getSystemService(Context.ACTIVITY_SERVICE);
+        Log.i("manju", "onCreate of service");
+        super.onCreate();
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.i("manju", "onStartCommand of service");
-		return START_STICKY;
-	}
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("manju", "onStartCommand of service");
+        return START_STICKY;
+    }
 
 
-	private void stopThread() {
-		if (mAppMoniterThread != null) {
-			mAppMoniterThread.interrupt();
-			mAppMoniterThread = null;
-		}
-	}
+    private void stopThread() {
+        if (mAppMoniterThread != null) {
+            mAppMoniterThread.interrupt();
+            mAppMoniterThread = null;
+        }
+    }
 
-	private void startThread() {
-		stopThread();
-		mAppMoniterThread = new AppMoniterThread(this);
-		mAppMoniterThread.start();
-		mAppMoniterThread.setName("AppMoniterThread");
-	}
+    private void startThread() {
+        stopThread();
+        mAppMoniterThread = new AppMoniterThread(this);
+        mAppMoniterThread.start();
+        mAppMoniterThread.setName("AppMoniterThread");
+    }
 }
 
 
--- app/src/main/java/com/example/manjunathnb/appstats/AppMoniterThread.java
+++ app/src/main/java/com/example/manjunathnb/appstats/AppMoniterThread.java
@@ -11,79 +11,75 @@ import android.util.Log;
 import com.example.manjunathnb.appstats.database.AppStatConstants;
 import com.example.manjunathnb.appstats.database.DBManager;
 
-class AppMoniterThread extends Thread {
+    class AppMoniterThread extends Thread {
 
-	private final AppMoniterService appMonitorService;
+        private final AppMoniterService appMonitorService;
 
-	AppMoniterThread(AppMoniterService appMoniterService) {
-		appMonitorService = appMoniterService;
-	}
+        AppMoniterThread(AppMoniterService appMoniterService) {
+            appMonitorService = appMoniterService;
+        }
 
-	@Override
-	public void run() {
-		while (true) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-			String foregroundTaskPackageName;
-			if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
-				ActivityManager.RunningTaskInfo foregroundTaskInfo = appMonitorService.mActivityManager.getRunningTasks(1).get(0);
-				foregroundTaskPackageName = foregroundTaskInfo.topActivity.getPackageName();
-			} else {
-				ActivityManager manager = (ActivityManager) appMonitorService.getSystemService(Context.ACTIVITY_SERVICE);
-				List<ActivityManager.RunningAppProcessInfo> tasks = manager.getRunningAppProcesses();
-				foregroundTaskPackageName = tasks.get(0).processName;
-			}
+                String foregroundTaskPackageName;
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+                    ActivityManager.RunningTaskInfo foregroundTaskInfo = appMonitorService.mActivityManager.getRunningTasks(1).get(0);
+                    foregroundTaskPackageName = foregroundTaskInfo.topActivity.getPackageName();
+                } else {
+                    ActivityManager manager = (ActivityManager) appMonitorService.getSystemService(Context.ACTIVITY_SERVICE);
+                    List<ActivityManager.RunningAppProcessInfo> tasks = manager.getRunningAppProcesses();
+                    foregroundTaskPackageName = tasks.get(0).processName;
+                }
 
-			if (foregroundTaskPackageName.equals(appMonitorService.previousPackageName)) {
+                if (foregroundTaskPackageName.equals(appMonitorService.previousPackageName)) {
 				/*    Log.i("manju :", " top package is same as previous package: TOP package :" + foregroundTaskPackageName + " and  previous package is" +
                         previousPackageName);*/
-				continue;
-			} else {
-				updateDatabase(foregroundTaskPackageName);
-			}
-		}
-	}
+                    continue;
+                } else {
+                    updateDatabase(foregroundTaskPackageName);
+                }
+            }
+        }
 
-	private void updateDatabase(String foregroundTaskPackageName) {
-		Log.i("manju :", " top package is NOT same as previous package: top package :" + foregroundTaskPackageName + " and  previous package is" +
-				appMonitorService.previousPackageName);
-		DBManager db = DBManager.getInstance(appMonitorService.getApplicationContext());
-		db.getWritableDB();
+        private void updateDatabase(String foregroundTaskPackageName) {
+            long insertedID = 0;
+            Log.i("manju :", " top package is NOT same as previous package: top package :" + foregroundTaskPackageName + " and  previous package is" +
+                    appMonitorService.previousPackageName);
+            DBManager db = DBManager.getInstance(appMonitorService.getApplicationContext());
+            db.getWritableDB();
 
-		/*if (isRequiredApp(foregroundTaskPackageName)) {*/
-		ContentValues cv = new ContentValues();
-		cv.put(AppMoniterService.LAST_APP_LAUNCHED_TIME, System.currentTimeMillis());
-		cv.put(AppMoniterService.APP_PACKAGE, foregroundTaskPackageName);
+            if (isRequiredApp(foregroundTaskPackageName)) {
+                ContentValues cv = new ContentValues();
+                cv.put(AppMoniterService.LAST_APP_LAUNCHED_TIME, System.currentTimeMillis());
+                cv.put(AppMoniterService.APP_PACKAGE, foregroundTaskPackageName);
+                insertedID = db.insert(AppStatConstants.APP_INFO_TABLE, cv);
+                Log.i("manju", " package inserted is :" + foregroundTaskPackageName + " inserted id is : " + insertedID);
+                Log.i("manju", "app launched is : " + foregroundTaskPackageName + " at time : " + System.currentTimeMillis());
+            }
+            if (isRequiredApp(appMonitorService.previousPackageName)) {
+                ContentValues cvU = new ContentValues();
+                cvU.put(AppMoniterService.LAST_APP_EXITED_TIME, System.currentTimeMillis());
+                long updatedID = db.update(AppStatConstants.APP_INFO_TABLE, cvU, AppMoniterService.ID + " = " + appMonitorService.previousinsertedID, null);
+                Log.i("manju ", "where clause was : " + AppMoniterService.ID + " = " + appMonitorService.previousinsertedID + " AND " + AppMoniterService.APP_PACKAGE + " = '" + appMonitorService.previousPackageName + "'");
+                Log.i("manju", "app exited is : " + appMonitorService.previousPackageName + " at time :" + System.currentTimeMillis());
+                Log.i("manju :", " return of update  :" + updatedID);
+            }
+            appMonitorService.previousinsertedID = insertedID;
+            appMonitorService.previousPackageName = foregroundTaskPackageName;
+        }
 
-		/*if (isRequiredApp(previousPackageName)) {*/
-		ContentValues cvU = new ContentValues();
-		cvU.put(AppMoniterService.LAST_APP_EXITED_TIME, System.currentTimeMillis());
 
-		db.beginTransaction();
-		
-		long insertedID = db.insert(AppStatConstants.APP_INFO_TABLE, cv);
-		Log.i("manju", " package inserted is :" + foregroundTaskPackageName + " inserted id is : " + insertedID);
-		Log.i("manju", "app launched is : " + foregroundTaskPackageName + " at time : " + System.currentTimeMillis());
-		
-		long updatedID = db.update(AppStatConstants.APP_INFO_TABLE, cvU, AppMoniterService.ID + " = " + appMonitorService.previousinsertedID, null);
-		Log.i("manju ", "where clause was : " + AppMoniterService.ID + " = " + appMonitorService.previousinsertedID + " AND " + AppMoniterService.APP_PACKAGE + " = '" + appMonitorService.previousPackageName + "'");
-		Log.i("manju", "app exited is : " + appMonitorService.previousPackageName + " at time :" + System.currentTimeMillis());
-		Log.i("manju :", " return of update  :" + updatedID);
-		
-		db.endTransaction();
-
-		appMonitorService.previousinsertedID = insertedID;
-		appMonitorService.previousPackageName = foregroundTaskPackageName;
-	}
-	
-	
-	private boolean isRequiredApp(String appPackage) {
-		return appPackage.equalsIgnoreCase("com.facebook.katana")
-				|| appPackage.equalsIgnoreCase("com.whatsapp")
-				|| appPackage.equalsIgnoreCase("com.android.contacts");
-	}
+        private boolean isRequiredApp(String appPackage) {
+       /* return appPackage.equalsIgnoreCase("com.facebook.katana")
+                || appPackage.equalsIgnoreCase("com.whatsapp")
+                || appPackage.equalsIgnoreCase("com.android.contacts");*/
+            return (!appPackage.equalsIgnoreCase("appstats.com.appstats"));
+        }
 }
